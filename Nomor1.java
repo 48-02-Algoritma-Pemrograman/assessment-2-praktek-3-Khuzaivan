3c34b8ef89e4cd0fe6518bd848ca49cc99f0883a
Khuzaivan <khuzaivan24@gmail.com>
2024-12-12 13:42:47 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -4,9 +4,20 @@ public class Nomor1 {
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai N: ");
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
-
+        for (int i = 1; i <= N; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
